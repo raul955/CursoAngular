300486c620a939a3740b7866fad5431e49592a5e
raul955 <43537133+raul955@users.noreply.github.com>
2019-11-22 08:26:04 +0100

    add all practicas

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CorreoComponent } from './Components/correo/correo.component';
+import { ListaCorreosComponent } from './Components/lista-correos/lista-correos.component';
+import { NuevoCorreoComponent } from './Components/nuevo-correo/nuevo-correo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CorreoComponent,
+    ListaCorreosComponent,
+    NuevoCorreoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
